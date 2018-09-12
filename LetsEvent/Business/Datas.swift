ce204abc84d2bbabf9b6c375f02bead5be6594e9
Renaud Marzana <renaud.marzana@alexiss-Mac.local>
2018-09-12 11:08:32 -0700

    models and populate

---
+++ LetsEvent/Business/Datas.swift
@@ -0,0 +1,54 @@
+//
+//  Datas.swift
+//  LetsEvent
+//
+//  Created by Renaud Marzana on 12/09/2018.
+//  Copyright © 2018 Renaud Marzana. All rights reserved.
+//
+
+import Foundation
+
+
+struct User{
+    
+    var email:String
+    var password:String
+    var idProfile:Int
+    var idEvents:Int = 0
+}
+
+struct Profile{
+    
+    var idProfile:Int
+    var nom:String
+    var prenom:String
+    var ddn:Date
+    var adresse:Adresse
+    var refUser:Int = 0
+}
+
+struct Adresse{
+    
+    var rue:String
+    var numero:String
+    var cp:Int
+    var localite:String
+    
+}
+
+struct Event{
+    
+    var nom:String
+    var categorie:Categorie
+    var description:String
+    var date:Date
+    var heure:String
+    var places:Int
+    var tarifs:[Double]
+}
+
+enum Categorie{
+    case Finance,Litterature,Cinema,Debats,Sport,Musique,Technologies,Multimedia,Cuisine
+}
+
+
